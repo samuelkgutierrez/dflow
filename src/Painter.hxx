b462ef5086b264963db19a18d4ec71a6d17e2b14
Samuel K. Gutierrez <samuel@lanl.gov>
2013-05-02 11:27:30 -0600

    add some files

---
+++ src/Painter.hxx
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2013 Samuel K. Gutierrez All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PAINTER_H_INCLUDED
+#define PAINTER_H_INCLUDED 
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <string>
+#include <iostream>
+
+#include "cgraph.h"
+#include "gvc.h"
+
+class Painter {
+private:
+    /* context */
+    GVC_t *gvc;
+    Agraph_t *g;
+
+public:
+    Painter(void);
+
+    ~Painter(void);
+};
+
+#endif
